69e81067a7363bf6a7776799c01ab0f0bec651b5
Sal Tarantola <starantola@gmail.com>
2019-03-30 18:47:41 +0000

    first committt

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+//import your own components
+import TrafficLight from "./component/traffic.jsx";
+
+//render your react application
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
